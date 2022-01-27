7ec22e792640480ab692efa601b3bb492f90a309
ozivarol <oguzhanvaroll43@gmail.com>
2022-01-27 21:12:48 +0300

    Validate

--- src/routers/Model.js
+++ src/routers/Model.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const ModelController = require("../controllers/ModelController")
-
+const authenticate = require("../middlewares/authenticate")
 
 const router = express.Router()
 
-router.route("/create").post(ModelController.create)
-router.route("/list").get(ModelController.index)
+router.route("/create").post(authenticate, ModelController.create)
+router.route("/list").get(authenticate, ModelController.index)
 
 
 
